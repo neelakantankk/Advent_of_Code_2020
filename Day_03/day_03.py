c5a4c4752e1d88c9ad2becdfa0f529a8a380f10c
Neelakantan K.K <neelakantankk@Terrapin-Station.Candyman>
2020-12-03 23:53:31 +0530

    Completed Day 3, part 2

--- Day_03/day_03.py
+++ Day_03/day_03.py
@@ -1,14 +1,12 @@
-def main():
-    with open('input','r') as infile:
-        lines = [line.strip() for line in infile]
-
+def get_trees(slope,lines):
     right,down = (0,0)
+    right_add, down_add = slope
 
     trees = 0
 
-    while down<len(lines) - 1:
-        right = right+3
-        down = down+1
+    while down<len(lines) - down_add:
+        right = right+right_add
+        down = down+down_add
 
         line = lines[down]
 
@@ -16,6 +14,21 @@ def main():
             right = right - len(line)
         if line[right] == '#':
             trees +=1
+
+    return trees
+
+
+def main():
+    with open('input','r') as infile:
+        lines = [line.strip() for line in infile]
+
+    slopes = [(1,1),(3,1),(5,1),(7,1),(1,2)]
+
+    trees = 1
+
+    for slope in slopes:
+        trees*=get_trees(slope,lines)
+
 
     print(trees)
 
