a7aa7eae0ce06d1e260c40126ceae56467bd6096
Neelakantan K.K <neelakantankk@Terrapin-Station.Candyman>
2020-12-06 20:03:58 +0530

    Completed Day 06, part 02

--- Day_06/day_06.py
+++ Day_06/day_06.py
@@ -1,11 +1,12 @@
+from functools import reduce
 def main():
     with open('input','r') as infile:
-        entries = [line.replace('\n','').strip() for line in infile.read().split("\n\n")]
+        entries = [[set(x) for x in line.strip().split("\n")] for line in infile.read().split("\n\n")]
 
     sum_counts = 0
 
     for entry in entries:
-        sum_counts += len(set(entry))
+        sum_counts += len(reduce(lambda x,y: x & y,entry))
 
     print(sum_counts)
 
