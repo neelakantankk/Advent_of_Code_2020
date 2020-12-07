b1e67c88143aa25582ac6316068ad415498cffa2
Neelakantan K.K <neelakantankk@Terrapin-Station.Candyman>
2020-12-07 23:49:04 +0530

    Completed Day 07, Part 02

--- Day_07/day_07.py
+++ Day_07/day_07.py
@@ -4,7 +4,7 @@ import pprint
 def parse_line(line):
     mo = re.search(r"^(.*?) bags contain (.*?)$", line)
     key = mo.group(1)
-    value = re.findall(r'\d+ (\w+ \w+) bag',mo.group(2))
+    value = re.findall(r'(\d+) (\w+ \w+) bag',mo.group(2))
     return (key,value)
 
 
@@ -19,13 +19,23 @@ def main():
     def contains_shiny_gold(bag):
         if rules[bag] == []:
             return False
-        elif 'shiny gold' in rules[bag]:
+        elif 'shiny gold' in [x[1] for x in rules[bag]]:
             return True
         else:
             contains_bag = False
             for inner_bag in rules[bag]:
-                contains_bag = contains_bag or contains_shiny_gold(inner_bag)
+                contains_bag = contains_bag or contains_shiny_gold(inner_bag[1])
             return contains_bag
+
+    def get_all_bags_in_bag(bag):
+        if rules[bag] == []:
+            return 0
+        else:
+            sum_bags = 0
+            for inner_bag in rules[bag]:
+                sum_bags += (int(inner_bag[0])*get_all_bags_in_bag(inner_bag[1])) 
+            return sum_bags + sum([int(bag[0]) for bag in rules[bag]])
+
 
     count_of_bags = 0
     for bag in rules.keys():
@@ -33,13 +43,7 @@ def main():
             count_of_bags+=1
 
     print(count_of_bags)
-
-
-
-
-
-
-
+    print(get_all_bags_in_bag('shiny gold'))
 
 if __name__ == '__main__':
     main()
