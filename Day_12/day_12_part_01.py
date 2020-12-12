ad50f96b821084ccc24af6599aca4dd7b509e065
Neelakantan K.K <neelakantankk@gmail.com>
2020-12-12 18:03:50 +0530

    Refactored Day 12, Part 01 as a vector addition and rotation solution

--- Day_12/day_12_part_01.py
+++ Day_12/day_12_part_01.py
@@ -1,54 +1,47 @@
 import math
 
 def main():
-    #filename = 'example.txt'
+    #filename = 'example'
     filename = 'input'
     with open(filename,'r') as infile:
         instructions = [(line[0],int(line.strip()[1:]))
                         for line in infile.readlines()]
 
-    # Model position on a grid, with N and S being +y and -y
-    # and E and W being +x and -x
 
+    # Boat position is modelled as a vector. The movement commands are
+    # modelled as unit vectors. Rotation commands affect only the unit
+    # vector 'F'. Coordinates are changed using vector addition.
     coords = [0,0]
-    # Direction is being modelled as a tuple where the
-    # first number says whether to change the x or y coord
-    # and the second number says whether to increase or
-    # decrease the coord
     movements = {
-        'N':(0,1),
-        'E':(1,1),
-        'S':(0,-1),
-        'W':(1,-1),
-        'F':(1,1)}
+        'N':(1,0),
+        'E':(0,1),
+        'S':(-1,0),
+        'W':(0,-1),
+        'F':(0,1)}
 
     for instruction,value in instructions:
-        if instruction in 'NSWEF':
-            axis,direction = movements[instruction]
-            coords[axis] += int((direction * value))
-        elif instruction in 'LR':
-            # Turns change the value of 'F', changing the sign of the coord
-            # according to the value of turn and the direction of turn
-            # 180 degrees flips the sign
-            # 90 and 270 degrees change it depending on the current sign and
-            # whether or not the turn is R or L
-            # the factors are all hand-calculated
+        if instruction in 'LR':
+            # 'F' is rotated using vector rotation.
+            # value is converted to radians. L is counterclockwise, R is clockwise
+            # Value is negative if rotation is counterclockwise, positive otherwise
+            # 'F' is then replaced with the rotated vector.
 
-            forwards = movements['F']
-            turn_factor = {'L':0,'R':1}
-            if value == 180:
-                movements['F'] = (forwards[0], int(forwards[1]*(-1)))
-            elif value == 90:
-                direction = 1 - forwards[0]
-                turn = int(forwards[1] * math.pow(
-                    -1,1 + forwards[0] - turn_factor[instruction]))
-                movements['F'] = (direction,turn)
-            elif value == 270:
-                direction = 1 - forwards[0]
-                turn = int(forwards[1] * math.pow(
-                    -1,2 + forwards[0] - turn_factor[instruction]))
-                movements['F'] = (direction,turn)
+            x1,y1 = movements['F']
+            if instruction == 'L':
+                value_rad = math.radians(value * (-1))
+            else:
+                value_rad = math.radians(value)
+            x2 = int((x1 * math.cos(value_rad)) - (y1 * math.sin(value_rad)))
+            y2 = int((x1 * math.sin(value_rad)) + (y1 * math.cos(value_rad)))
+            movements['F'] = (x2,y2)
+        else:
+            x,y = movements[instruction]
+            x1 = x*value
+            y1 = y*value
+            coords[0] += x1
+            coords[1] += y1
 
+    # Manhattan distance
     print(abs(coords[0]) + abs(coords[1]))
 
 if __name__ == '__main__':
